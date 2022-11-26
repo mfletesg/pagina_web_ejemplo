0c14531a5380b1ed930453cf8e5bbbec59ac23cb
Miguel Fletes <mfletesg@gmail.com>
2022-11-26 10:07:07 -0600

    add function js

--- usuario.js
+++ usuario.js
@@ -1,3 +1,46 @@
-function btnpush(){
-    alert('Hola Alejandro')
+window.addEventListener("load", function(event) {
+    let fruta = '🍎';
+    btnpush(fruta)
+});
+
+
+
+function btnpush(fruta){
+    switch (fruta) {
+        case '🍎':
+            console.log('Es una manzana ' + fruta)
+            break;
+        case '🍏':
+            console.log('Es una manzana verde ' + fruta)
+            break;
+        case '🥑':
+            console.log('Es una aguacate ' + fruta)
+            break;
+        default:
+            console.log('No es ninguna fruta')
+            break;
+    }
+
+        /*
+    if(fruta === '🍎'){
+        console.log('Es una manzana ' + fruta)
+    }
+    else{
+        console.log('No es una manzana')
+    }*/
+
+
+    let array = ['🍎', '🍏', '🥑', '🍉']
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] === '🥑'){
+            console.log(array[i])
+            document.getElementById('emojiFruta').innerHTML = array[i];
+            break;
+            
+        }
+    }
+
+
+    //console.log(array)
+
 }
